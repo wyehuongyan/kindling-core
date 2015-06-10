288fc4f81caee5d9f114716ed04d150195d5d078
Yan Wye Huong <wyehuong88@gmail.com>
2015-06-11 00:52:09 +0800

    laravel 5.1 upgrade: http://laravel.com/docs/master/upgrade#upgrade-5.1.0

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -112,4 +112,34 @@ class AuthController extends Controller {
 
         return response()->json($success)->setCallback($request->input('callback'));
     }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    public function validator(array $data)
+    {
+        return Validator::make($data, [
+            'username' => 'required|max:255|unique:users',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|confirmed|min:6',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    public function create(array $data)
+    {
+        return User::create([
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
 }
