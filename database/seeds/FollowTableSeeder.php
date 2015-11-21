a66ff0db5517a78fe1c4ae1874a1bfe3bd87ce03
Yan Wye Huong <wyehuong88@gmail.com>
2015-11-21 17:24:29 +0800

    users follow each other.

--- database/seeds/FollowTableSeeder.php
+++ database/seeds/FollowTableSeeder.php
@@ -55,6 +55,7 @@ class FollowTableSeeder extends Seeder {
         $user_15->following()->save($user_15);
 
         // all of them will follow sprubix (production)
+        //// user 1
         $user_2->following()->save($user_1);
         $user_3->following()->save($user_1);
         $user_4->following()->save($user_1);
@@ -69,6 +70,199 @@ class FollowTableSeeder extends Seeder {
         $user_13->following()->save($user_1);
         $user_14->following()->save($user_1);
         $user_15->following()->save($user_1);
+
+        // everyone follows each other
+        //// user 4
+        $user_1->following()->save($user_4);
+        $user_2->following()->save($user_4);
+        $user_3->following()->save($user_4);
+        $user_5->following()->save($user_4);
+        $user_6->following()->save($user_4);
+        $user_7->following()->save($user_4);
+        $user_8->following()->save($user_4);
+        $user_9->following()->save($user_4);
+        $user_10->following()->save($user_4);
+        $user_11->following()->save($user_4);
+        $user_12->following()->save($user_4);
+        $user_13->following()->save($user_4);
+        $user_14->following()->save($user_4);
+        $user_15->following()->save($user_4);
+
+        //// user 5
+        $user_1->following()->save($user_5);
+        $user_2->following()->save($user_5);
+        $user_3->following()->save($user_5);
+        $user_4->following()->save($user_5);
+        $user_6->following()->save($user_5);
+        $user_7->following()->save($user_5);
+        $user_8->following()->save($user_5);
+        $user_9->following()->save($user_5);
+        $user_10->following()->save($user_5);
+        $user_11->following()->save($user_5);
+        $user_12->following()->save($user_5);
+        $user_13->following()->save($user_5);
+        $user_14->following()->save($user_5);
+        $user_15->following()->save($user_5);
+
+        //// user 6
+        $user_1->following()->save($user_6);
+        $user_2->following()->save($user_6);
+        $user_3->following()->save($user_6);
+        $user_4->following()->save($user_6);
+        $user_5->following()->save($user_6);
+        $user_7->following()->save($user_6);
+        $user_8->following()->save($user_6);
+        $user_9->following()->save($user_6);
+        $user_10->following()->save($user_6);
+        $user_11->following()->save($user_6);
+        $user_12->following()->save($user_6);
+        $user_13->following()->save($user_6);
+        $user_14->following()->save($user_6);
+        $user_15->following()->save($user_6);
+
+        //// user 7
+        $user_1->following()->save($user_7);
+        $user_2->following()->save($user_7);
+        $user_3->following()->save($user_7);
+        $user_4->following()->save($user_7);
+        $user_5->following()->save($user_7);
+        $user_6->following()->save($user_7);
+        $user_8->following()->save($user_7);
+        $user_9->following()->save($user_7);
+        $user_10->following()->save($user_7);
+        $user_11->following()->save($user_7);
+        $user_12->following()->save($user_7);
+        $user_13->following()->save($user_7);
+        $user_14->following()->save($user_7);
+        $user_15->following()->save($user_7);
+
+        //// user 8
+        $user_1->following()->save($user_8);
+        $user_2->following()->save($user_8);
+        $user_3->following()->save($user_8);
+        $user_4->following()->save($user_8);
+        $user_5->following()->save($user_8);
+        $user_6->following()->save($user_8);
+        $user_7->following()->save($user_8);
+        $user_9->following()->save($user_8);
+        $user_10->following()->save($user_8);
+        $user_11->following()->save($user_8);
+        $user_12->following()->save($user_8);
+        $user_13->following()->save($user_8);
+        $user_14->following()->save($user_8);
+        $user_15->following()->save($user_8);
+
+        //// user 9
+        $user_1->following()->save($user_9);
+        $user_2->following()->save($user_9);
+        $user_3->following()->save($user_9);
+        $user_4->following()->save($user_9);
+        $user_5->following()->save($user_9);
+        $user_6->following()->save($user_9);
+        $user_7->following()->save($user_9);
+        $user_8->following()->save($user_9);
+        $user_10->following()->save($user_9);
+        $user_11->following()->save($user_9);
+        $user_12->following()->save($user_9);
+        $user_13->following()->save($user_9);
+        $user_14->following()->save($user_9);
+        $user_15->following()->save($user_9);
+
+        //// user 10
+        $user_1->following()->save($user_10);
+        $user_2->following()->save($user_10);
+        $user_3->following()->save($user_10);
+        $user_4->following()->save($user_10);
+        $user_5->following()->save($user_10);
+        $user_6->following()->save($user_10);
+        $user_7->following()->save($user_10);
+        $user_8->following()->save($user_10);
+        $user_9->following()->save($user_10);
+        $user_11->following()->save($user_10);
+        $user_12->following()->save($user_10);
+        $user_13->following()->save($user_10);
+        $user_14->following()->save($user_10);
+        $user_15->following()->save($user_10);
+
+        //// user 11
+        $user_1->following()->save($user_11);
+        $user_2->following()->save($user_11);
+        $user_3->following()->save($user_11);
+        $user_4->following()->save($user_11);
+        $user_5->following()->save($user_11);
+        $user_6->following()->save($user_11);
+        $user_7->following()->save($user_11);
+        $user_8->following()->save($user_11);
+        $user_9->following()->save($user_11);
+        $user_10->following()->save($user_11);
+        $user_12->following()->save($user_11);
+        $user_13->following()->save($user_11);
+        $user_14->following()->save($user_11);
+        $user_15->following()->save($user_11);
+
+        //// user 12
+        $user_1->following()->save($user_12);
+        $user_2->following()->save($user_12);
+        $user_3->following()->save($user_12);
+        $user_4->following()->save($user_12);
+        $user_5->following()->save($user_12);
+        $user_6->following()->save($user_12);
+        $user_7->following()->save($user_12);
+        $user_8->following()->save($user_12);
+        $user_9->following()->save($user_12);
+        $user_10->following()->save($user_12);
+        $user_11->following()->save($user_12);
+        $user_13->following()->save($user_12);
+        $user_14->following()->save($user_12);
+        $user_15->following()->save($user_12);
+
+        //// user 13
+        $user_1->following()->save($user_13);
+        $user_2->following()->save($user_13);
+        $user_3->following()->save($user_13);
+        $user_4->following()->save($user_13);
+        $user_5->following()->save($user_13);
+        $user_6->following()->save($user_13);
+        $user_7->following()->save($user_13);
+        $user_8->following()->save($user_13);
+        $user_9->following()->save($user_13);
+        $user_10->following()->save($user_13);
+        $user_11->following()->save($user_13);
+        $user_12->following()->save($user_13);
+        $user_14->following()->save($user_13);
+        $user_15->following()->save($user_13);
+
+        //// user 14
+        $user_1->following()->save($user_14);
+        $user_2->following()->save($user_14);
+        $user_3->following()->save($user_14);
+        $user_4->following()->save($user_14);
+        $user_5->following()->save($user_14);
+        $user_6->following()->save($user_14);
+        $user_7->following()->save($user_14);
+        $user_8->following()->save($user_14);
+        $user_9->following()->save($user_14);
+        $user_10->following()->save($user_14);
+        $user_11->following()->save($user_14);
+        $user_12->following()->save($user_14);
+        $user_13->following()->save($user_14);
+        $user_15->following()->save($user_14);
+
+        //// user 15
+        $user_1->following()->save($user_15);
+        $user_2->following()->save($user_15);
+        $user_3->following()->save($user_15);
+        $user_4->following()->save($user_15);
+        $user_5->following()->save($user_15);
+        $user_6->following()->save($user_15);
+        $user_7->following()->save($user_15);
+        $user_8->following()->save($user_15);
+        $user_9->following()->save($user_15);
+        $user_10->following()->save($user_15);
+        $user_11->following()->save($user_15);
+        $user_12->following()->save($user_15);
+        $user_13->following()->save($user_15);
+        $user_14->following()->save($user_15);
 
         $user_1->save();
         $user_2->save();
