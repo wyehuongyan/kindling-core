93b0712c7d0fd4eb1264314d2b8a931cacb9406b
Wah Chin Han <onishinobimusha@hotmail.com>
2016-01-05 18:42:08 +0800

    Add newly registered users to the MailChimp Subscriber List.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -190,6 +190,9 @@ class AuthController extends Controller {
 
             SprubixQueue::queueVerificationEmail($user);
 
+            // Mailchimp add current user as subscriber
+            $this->mailchimpAddSubscriber($request['email'], $request['username']);
+
             return response()->json($success)->setCallback($request->input('callback'));
         }
 
@@ -322,6 +325,40 @@ class AuthController extends Controller {
         }
 
         return response()->json($json)->setCallback($request->input('callback'));
+    }
+
+    // Mailchimp: Add subscriber to List
+    public function mailchimpAddSubscriber($email, $username) {
+        $apiKey = env('MAILCHIMP_KEY');
+        $listId = env('MAILCHIMP_LIST');
+
+        $memberId = md5(strtolower($email));
+        $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+        $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listId . '/members/' . $memberId;
+
+        $json = json_encode([
+            'email_address' => $email,
+            'status'        => "subscribed", // "subscribed","unsubscribed","cleaned","pending"
+            'merge_fields'  => [
+                'UNAME'     => $username
+            ]
+        ]);
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+
+        $result = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return $httpCode;
     }
 
     /**
